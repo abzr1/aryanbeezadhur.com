4ee1bd08a54c478ee2c6b172b1eeac62f3d00693
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2020-10-15 21:19:46 +0100

    Added message.svg for livechat

--- script.js
+++ script.js
@@ -6,3 +6,5 @@ AOS.init({
 document.getElementById('hire-now').addEventListener('click', () => {
     window.open('https://freelancer.com/u/aryanbeezadhur')
 })
+
+document.getElementsByClassName('gs4')[0].outerHTML = `<img class="gs4" src="assets/message.svg" />`

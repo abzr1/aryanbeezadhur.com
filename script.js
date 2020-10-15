6414bba9ba06d2646c0dc31fda3c6a7fec49c0c9
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2020-10-15 21:25:37 +0100

    Revert "Added message.svg for livechat"
    This reverts commit 4ee1bd08a54c478ee2c6b172b1eeac62f3d00693.

--- script.js
+++ script.js
@@ -6,5 +6,3 @@ AOS.init({
 document.getElementById('hire-now').addEventListener('click', () => {
     window.open('https://freelancer.com/u/aryanbeezadhur')
 })
-
-document.getElementsByClassName('gs4')[0].outerHTML = `<img class="gs4" src="assets/message.svg" />`

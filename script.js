124e836f7b262a0641a6e2ef6f58126ef103def2
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2020-11-03 21:10:44 +0000

    Added navbar, updated UI, meta and GS
    - Added navbar
    - Changed description in meta and .top-header to "Junior web developer"
    - Moved social icons to footer
    - Anonymize IP addresses in GoSquared

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+const $hireNowBtn = document.getElementById('hire-now')
+const $navbarHireBtn = document.getElementById('navbar-hire-btn')
+
 AOS.init({
     duration: 1000,
     once: true
 })
+/*
+$navbarHireBtn.onclick = () => {
+    _gs('event', 'Hire button clicked')
+    window.open('https://freelancer.com/u/aryanbeezadhur')
+}
 
-document.getElementById('hire-now').addEventListener('click', () => {
+$hireNowBtn.addEventListener('click', () => {
     _gs('event', 'Hire button clicked')
     window.open('https://freelancer.com/u/aryanbeezadhur')
 })
+*/

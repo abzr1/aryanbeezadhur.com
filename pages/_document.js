c3ffce5e5d634784a778f68fe3f56d5035bcaae7
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2021-11-01 18:09:15 +0000

    Remove Microsoft Clarity

--- pages/_document.js
+++ pages/_document.js
@@ -18,18 +18,6 @@ export default class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&family=DM+Serif+Display&display=swap"
                         rel="stylesheet"
                     />
-
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
-                                (function(c,l,a,r,i,t,y){
-                                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                                })(window, document, "clarity", "script", "94pmxvuveq");
-                            `
-                        }}
-                    />
                 </Head>
 
                 <body>

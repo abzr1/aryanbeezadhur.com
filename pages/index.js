0acc9c8e97751ea466d69519e7117f60f5853282
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2022-01-18 22:17:55 +0000

    Revert "Add links to skills"
    This reverts commit fb3edfd95862905c097c61c6dee18cdf21fd4b82.

--- pages/index.js
+++ pages/index.js
@@ -24,79 +24,9 @@ export default function Index() {
             </p>
 
             <p>
-                Tech: HTML,{' '}
-                <a
-                    href="https://htmx.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    HTMX
-                </a>
-                , CSS,{' '}
-                <a
-                    href="https://tailwindcss.com"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Tailwind CSS
-                </a>
-                , JavaScript,{' '}
-                <a
-                    href="https://alpinejs.dev"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Alpine.js
-                </a>
-                ,{' '}
-                <a
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    React.js
-                </a>
-                ,{' '}
-                <a
-                    href="https://nextjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Next.js
-                </a>
-                ,{' '}
-                <a
-                    href="https://svelte.dev"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Svelte
-                </a>
-                ,{' '}
-                <a
-                    href="https://kit.svelte.dev"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    SvelteKit
-                </a>
-                , Python,{' '}
-                <a
-                    href="https://djangoframework.com"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Django
-                </a>
-                , PHP,{' '}
-                <a
-                    href="https://laravel.com"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Laravel
-                </a>
-                .
+                Tech: HTML, HTMX, CSS, Tailwind CSS, JavaScript, Alpine.js,
+                React.js, Next.js, Svelte, SvelteKit, Python, Django, PHP,
+                Laravel.
             </p>
 
             <nav>

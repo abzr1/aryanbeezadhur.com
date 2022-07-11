b2bf1b2955a313cad4d05723d9175aaece381150
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2022-07-11 10:28:23 +0100

    Soften text color

--- pages/_document.js
+++ pages/_document.js
@@ -20,7 +20,7 @@ export default class MyDocument extends Document {
                     />
                 </Head>
 
-                <body className="h-full bg-stone-900 text-white text-lg">
+                <body className="h-full bg-stone-900 text-stone-50 text-lg">
                     <Main />
                     <NextScript />
                 </body>

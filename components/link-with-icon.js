a068fe4c84185c5a9e5c185e0073d5cfd6d6317f
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2022-08-17 15:30:26 +0100

    Add margin to <LinkWithIcon />

--- components/link-with-icon.js
+++ components/link-with-icon.js
@@ -1,6 +1,6 @@
 export default function LinkWithIcon({ children, ...props }) {
     return (
-        <a className="inline-flex items-center gap-1" {...props}>
+        <a className="my-2 inline-flex items-center gap-1" {...props}>
             {children}
         </a>
     )

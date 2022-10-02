68d9cd28c2282e2233af407e6e64c69bac8b29a6
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2022-10-02 11:21:36 +0100

    Change font to Chakra Petch

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     theme: {
         extend: {},
         fontFamily: {
-            sans: ['Fira Sans', ...defaultTheme.fontFamily.sans]
+            sans: ['Chakra Petch', ...defaultTheme.fontFamily.sans]
         }
     },
     plugins: []

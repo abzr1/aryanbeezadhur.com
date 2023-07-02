7dc317be0ffe1601b4095416ad6e2ccc86b6187e
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2023-07-02 16:16:57 +0100

    Update /portfolio

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -6,19 +6,18 @@ import Paragraph from '../components/paragraph'
 const items = [
     {
         title: 'atyb4',
-        description:
-            'My portfolio site built with React and Next.js, deployed with Vercel.',
+        description: 'My personal site, built with Next.js.',
         url: 'https://github.com/atyb4/atyb4'
     },
     {
         title: 'wordstats',
         description:
-            'A web tool that display text stats, built with Alpine.js and Tailwind CSS.',
+            'A website that calculates text stats, built with Next.js.',
         url: 'https://github.com/atyb4/wordstats'
     },
     {
         title: 'project_euler',
-        description: 'My solutions to Project Euler',
+        description: 'My solutions to Project Euler.',
         url: 'https://github.com/atyb4/project_euler'
     }
 ]
